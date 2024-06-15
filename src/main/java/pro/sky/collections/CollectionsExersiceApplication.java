c712027dbe011e49796672de104a8fb1b7c762af
freddycooper <almosttanonymous@gmail.com>
2024-06-15 22:02:54 +0300

    Initial commit.

---
+++ src/main/java/pro/sky/collections/CollectionsExersiceApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsExersiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsExersiceApplication.class, args);
+	}
+
+}
